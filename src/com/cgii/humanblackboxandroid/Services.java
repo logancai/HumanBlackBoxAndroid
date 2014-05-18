e51c766eda7b19ae7a9284ff4336abbc9901a674
Logan Cai <logan.cai@gmail.com>
2014-05-18 13:21:03 -0700

    Added Padding

---
+++ src/com/cgii/humanblackboxandroid/Services.java
@@ -0,0 +1,20 @@
+package com.cgii.humanblackboxandroid;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
+public class Services extends Service{
+
+	@Override
+	public IBinder onBind(Intent intent) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	@Override
+	public void onCreate(){
+		
+	}
+	
+}
